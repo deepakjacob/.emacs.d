ea753d4dfa6797d7c30fc735f6ba891076df7e95
Jacob Deepak <deepak.jacob@telus.com>
2017-10-15 14:52:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# this is a React/Node/JSON/Clojure-friendly emacs config
+
+
+## Installing
+
+1. Close Emacs.
+2. Delete `~/.emacs` or `~/.emacs.d` if they exist. (Windows users, your
+   emacs files will probably live in
+   `C:\Users\your_user_name\AppData\Roaming\`. So, for example, you
+   would delete `C:\Users\jason\AppData\Roaming\.emacs.d`.) This is
+   where Emacs looks for configuration files, and deleting these files
+   and directories will ensure that you start with a clean slate.
+3. Clone the .emacs.d from https://github.com/deepakjacob. Its contents should be a folder and place this folder as ~/.emacs.d
+
+Then open Emacs.
+
+That should install the latest version. Enjoy!
+
+## Organization
+
+I've tried to separate everything logically and document the purpose
+of every line. [`init.el`](./init.el) acts as a kind of table of
+contents.  It's a good idea to eventually go through `init.el` and the
+files under the `customizations` directory so that you know exactly
+what's going on.
+
---
+++ customizations/ui.el
@@ -0,0 +1,81 @@
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements. Some useful customizations are
+;; commented out, and begin with the line "CUSTOMIZE". These are more
+;; a matter of preference and may require some fiddling to match your
+;; preferences
+
+;; Turn off the menu bar at the top of each frame because it's distracting
+(menu-bar-mode -1)
+;; Turn of the tool bar at the top of the each frame because it's distracting
+(tool-bar-mode -1)
+
+;; Chnage the font to Operator Mono
+(set-face-attribute 'default nil :font "Operator Mono") ;;default font
+;; increase font size for better readability
+(set-face-attribute 'default nil :height 140)
+
+(setq-default cursor-type 'bar) ;;make cursor a line
+
+;;default starting emacs size
+(add-to-list 'default-frame-alist '(height . 40))
+(add-to-list 'default-frame-alist '(width . 80))
+
+;; Show line numbers
+(global-linum-mode)
+
+;; Color Themes
+;; Read http://batsov.com/articles/2012/02/19/color-theming-in-emacs-reloaded/
+;; for a great explanation of emacs color themes.
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Custom-Themes.html
+;; for a more technical explanation.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'load-path "~/.emacs.d/themes")
+(load-theme 'spacemacs-dark t)
+
+;; Uncomment the lines below by removing semicolons and play with the
+;; values in order to set the width (in characters wide) and height
+;; (in lines high) Emacs will have whenever you start it
+;; (setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
+
+;; These settings relate to how emacs interacts with your operating system
+(setq ;; makes killing/yanking interact with the clipboard
+      x-select-enable-clipboard t
+
+      ;; I'm actually not sure what this does but it's recommended?
+      x-select-enable-primary t
+
+      ;; Save clipboard strings into kill ring before replacing them.
+      ;; When one selects something in another program to paste it into Emacs,
+      ;; but kills something in Emacs before actually pasting it,
+      ;; this selection is gone unless this variable is non-nil
+      save-interprogram-paste-before-kill t
+
+      ;; Shows all options when running apropos. For more info,
+      ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Apropos.html
+      apropos-do-all t
+
+      ;; Mouse yank commands yank at point instead of at click.
+      mouse-yank-at-point t)
+
+;; No cursor blinking, it's distracting
+(blink-cursor-mode 0)
+
+(global-prettify-symbols-mode +1)
+
+;; full path in title bar
+(setq-default frame-title-format '("")) ;;frame title
+;;(setq-default frame-title-format "%b (%f)")
+
+;; don't pop up font menu
+(global-set-key (kbd "s-t") '(lambda () (interactive)))
+
+;; no bell
+(setq ring-bell-function 'ignore)
+
+(require 'magit)
+(define-key global-map (kbd "C-c m") 'magit-status)
+
+;; Don't show native OS scroll bars for buffers because they're redundant
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
