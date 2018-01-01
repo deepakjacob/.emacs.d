c5894870f26edd529f59486dec573df76e7e0b50
jacobdeepak <deepak.jacob@telus.com>
2018-01-01 15:53:03 -0500

    Modeline to powerline

--- init.el
+++ init.el
@@ -96,7 +96,7 @@
     neotree
 
     ;;  mode line config
-    spaceline
+    powerline
 
     ;; nice lines
     page-break-lines
@@ -275,12 +275,6 @@
                   '(lambda () (interactive) (global-text-scale-adjust -1)))
 
 
-(require 'spaceline-config)
-(spaceline-emacs-theme)
-(spaceline-info-mode)
-
-
-
 (require 'diminish)
 (eval-after-load "projectile"
   '(diminish 'projectile-mode))
@@ -320,61 +314,21 @@
 (eval-after-load 'auto-revert-mode
   '(diminish 'auto-revert-mode))
 
-(require 'spaceline-config)
-(spaceline-emacs-theme)
-(spaceline-info-mode)
-(setq powerline-arrow-shape 'arrow)   ;; the default
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line ((t (:foreground "#f9f9f9" :background "gray0" :box nil))))
- '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil))))
- '(powerline-active1 ((t (:inherit mode-line :background "DarkOrange3"))))
- '(powerline-active2 ((t (:inherit mode-line :background "gray17" :foreground "white"))))
- '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "gray17")))))
+(require 'powerline)
+(powerline-default-theme)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#1B2229" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#DFDFDF"])
- '(custom-safe-themes
-   (quote
-    ("a7e7804313dbf827a441c86a8109ef5b64b03011383322cbdbf646eb02692f76" "42b8102c1234a9f680722953161c1127cc59ec68ad8d5c710af60d68c3b6e6ef" default)))
- '(fci-rule-color "#5B6268")
- '(jdee-db-active-breakpoint-face-colors (cons "#1B2229" "#51afef"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#1B2229" "#98be65"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
- '(linum-format " %3i ")
- '(org-fontify-done-headline t)
- '(org-fontify-quote-and-verse-blocks t)
- '(org-fontify-whole-heading-line t)
  '(powerline-default-separator (quote slant))
  '(powerline-gui-use-vcs-glyph t)
- '(vc-annotate-background "#1B2229")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#98be65")
-    (cons 40 "#b4be6c")
-    (cons 60 "#d0be73")
-    (cons 80 "#ECBE7B")
-    (cons 100 "#e6ab6a")
-    (cons 120 "#e09859")
-    (cons 140 "#da8548")
-    (cons 160 "#d38079")
-    (cons 180 "#cc7cab")
-    (cons 200 "#c678dd")
-    (cons 220 "#d974b7")
-    (cons 240 "#ec7091")
-    (cons 260 "#ff6c6b")
-    (cons 280 "#cf6162")
-    (cons 300 "#9f585a")
-    (cons 320 "#6f4e52")
-    (cons 340 "#5B6268")
-    (cons 360 "#5B6268")))
- '(vc-annotate-very-old-color nil))
+ '(powerline-image-apple-rgb t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(powerline-active1 ((t (:inherit mode-line :background "firebrick" :foreground "white"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "gray17" :foreground "white"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "chocolate")))))
