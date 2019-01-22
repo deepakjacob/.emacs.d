3cd88b90fb617fa922e0d639f0b05eaccc0fdbb4
Jacob Deepak <jacobdeepak@MacBook-Pro.local>
2019-01-22 02:05:08 -0500

    misc fixes

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
    ["#000000" "#C16069" "#A2BF8A" "#ECCC87" "#80A0C2" "#B58DAE" "#86C0D1" "#dfdfdf"])
  '(custom-safe-themes
    (quote
-    ("100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" default)))
+    ("c9b89349d269af4ac5d832759df2f142ae50b0bbbabcce9c0dd53f79008443c9" "bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "100e7c5956d7bb3fd0eebff57fde6de8f3b9fafa056a2519f169f85199cc1c96" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "75d3dde259ce79660bac8e9e237b55674b910b470f313cdf4b019230d01a982a" default)))
  '(doom-neotree-folder-size 1.0)
  '(fci-always-use-textual-rule nil)
  '(fci-rule-color "light green")
@@ -30,8 +30,16 @@
  '(neo-window-width 35)
  '(package-selected-packages
    (quote
-    (multi-term emojify eslint-fix dart-mode dracula-theme ace-jump-buffer ace-jump-mode badger-theme atom-dark-theme atom-one-dark-theme bubbleberry-theme ujelly-theme docker-compose-mode nodejs-repl indium eglot easy-kill-extras easy-kill doom-modeline zoom zerodark-theme yaml-mode winum use-package tagedit spacemacs-theme smooth-scrolling smex smartparens rjsx-mode rainbow-delimiters projectile php-mode paredit page-break-lines neotree move-dup markdown-mode json-mode js2-refactor jbeans-theme highlight-symbol highlight-indent-guides guide-key go-guru go-autocomplete focus fill-column-indicator exec-path-from-shell ergoemacs-status ergoemacs-mode dumb-jump doom-themes diminish counsel clojure-mode-extra-font-locking cider change-inner browse-kill-ring base16-theme alchemist)))
+    (aggressive-indent multi-term emojify eslint-fix dart-mode dracula-theme ace-jump-buffer ace-jump-mode badger-theme atom-dark-theme atom-one-dark-theme bubbleberry-theme ujelly-theme docker-compose-mode nodejs-repl indium eglot easy-kill-extras easy-kill doom-modeline zoom zerodark-theme yaml-mode winum use-package tagedit spacemacs-theme smooth-scrolling smex smartparens rjsx-mode rainbow-delimiters projectile php-mode paredit page-break-lines neotree move-dup markdown-mode json-mode js2-refactor jbeans-theme highlight-symbol highlight-indent-guides guide-key go-guru go-autocomplete focus fill-column-indicator exec-path-from-shell ergoemacs-status ergoemacs-mode dumb-jump doom-themes diminish counsel clojure-mode-extra-font-locking cider change-inner browse-kill-ring base16-theme alchemist)))
  '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]])
  '(vc-annotate-background "#282c34")
  '(vc-annotate-color-map
    (list
