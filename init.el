6792bad35186b609dd4605e17e3333b69c493a92
Deepak Jacob <deepak.jacob@gmail.com>
2024-03-29 19:51:48 -0400

    added .gitignore

--- init.el
+++ init.el
@@ -174,3 +174,16 @@
   ;;disable splash screen and startup message
 (setq inhibit-startup-message t) 
 (setq initial-scratch-message nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-mode company rustic general which-key ivy-posframe ivy mini-frame projectile evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
