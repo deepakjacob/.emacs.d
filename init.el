9c32d5ea1a29926e6ecaba1840bdb7d79ba60c0a
Deepak Jacob <deepak.jacob@gmail.com>
2024-04-14 14:14:11 -0400

    kind-icons for corfu

--- init.el
+++ init.el
@@ -33,7 +33,8 @@
       
       orderless
       cape
-      
+      kind-icon
+      svg-lib 
       
   ))
 
@@ -143,9 +144,6 @@
   ;; setting is useful beyond Corfu.
   (setq read-extended-command-predicate #'command-completion-default-include-p))
   ;; Enable auto completion and configure quitting
-
-
-  
 (setq corfu-auto t
       corfu-quit-no-match 'separator) ;; or t
 
@@ -196,6 +194,31 @@
   :commands lsp)
 
 (use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package kind-icon
+  :after corfu
+  :custom
+  (kind-icon-use-icons t)
+  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
+  (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
+  (kind-icon-blend-frac 0.08)
+
+  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
+  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
+  ;; directory to a location appropriate to `no-littering' conventions, a
+  ;; package which moves directories of other packages to sane locations.
+  (svg-lib-icons-dir "svg-lib/cache/") ; Change cache dir
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
+
+  ;; Add hook to reset cache so the icon colors match my theme
+  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
+  ;; the theme using my custom defined command for switching themes. If I don't
+  ;; do this, then the backgound color will remain the same, meaning it will not
+  ;; match the background color corresponding to the current theme. Important
+  ;; since I have a light theme and dark theme I switch between. This has no
+  ;; function unless you use something similar
+  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
 
 
 (setq custom-file (expand-file-name "emacs-custom.el" temporary-file-directory))
